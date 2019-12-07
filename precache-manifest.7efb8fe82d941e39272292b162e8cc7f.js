631623b6f9e3929bb26cb77993b22c41ac077966
tomrennhack <tomrennhack@gmail.com>
2019-12-07 09:59:36 -0500

    Updates

---
+++ precache-manifest.7efb8fe82d941e39272292b162e8cc7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a36631ebf166c24f7fa0732b300f48",
+    "url": "/clicky-game-renns/index.html"
+  },
+  {
+    "revision": "4b82c6f0e4abddd606d3",
+    "url": "/clicky-game-renns/static/css/main.9bcbbf92.chunk.css"
+  },
+  {
+    "revision": "b75e3d353c24e5a2e4ff",
+    "url": "/clicky-game-renns/static/js/2.a3d3acd8.chunk.js"
+  },
+  {
+    "revision": "4b82c6f0e4abddd606d3",
+    "url": "/clicky-game-renns/static/js/main.2325d55e.chunk.js"
+  },
+  {
+    "revision": "df35173308042731029f",
+    "url": "/clicky-game-renns/static/js/runtime-main.8dd0d515.js"
+  }
+]);
